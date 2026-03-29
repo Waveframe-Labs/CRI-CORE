3f8c051d7097dd979125f2bce69282b0d765c67c
Shawn C. Wright <swright@waveframelabs.org>
2026-03-29 19:38:50 -0400

    feat(api): expose evaluate() in public package surface

--- src/cricore/__init__.py
+++ src/cricore/__init__.py
@@ -8,13 +8,15 @@ from importlib.metadata import version, PackageNotFoundError
 try:
     __version__ = version("cricore")
 except PackageNotFoundError:
-    __version__ = "0.8.0"
+    __version__ = "0.10.0"
 
 
 # Public API surface
 from .enforcement.execution import run_enforcement_pipeline
+from .api import evaluate
 
 __all__ = [
     "__version__",
     "run_enforcement_pipeline",
+    "evaluate",
 ]
